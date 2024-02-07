c8fb3392931c73aeb8648b08565306f5db8944cd
nurseda-diker <nursedadiker760@gmail.com>
2024-02-07 18:12:42 +0300

    add project files

---
+++ WorkAround/Program.cs
@@ -0,0 +1,31 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Business.Concrete;
+using Entities.Concrete;
+
+Console.WriteLine("Hello, World!");
+SelamVer();
+SelamVer2();
+Console.ReadLine();
+
+
+
+ static void SelamVer()
+{
+    Console.WriteLine("hello");
+}
+
+void SelamVer2()
+{
+    Console.WriteLine("hello2");
+}
+
+Person person = new Person();
+person.FirstName = "Nur";
+person.LastName = "Diker";
+person.DateOfBirth = 2000;
+person.NationalIdentity = 123;
+
+PttManager personManager = new PttManager(new PersonManager());
+personManager.GiveMask(person);
+
+
